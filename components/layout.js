4865fcc55e8b93246ae2fc972bd0adabb048f827
tkpremier <tk@lmbrjck.com>
2020-10-03 20:21:35 -0400

    init finals...here we go tkpremier

---
+++ components/layout.js
@@ -0,0 +1,60 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+
+const name = 'Tommy Kim';
+
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header} >
+        {home
+          ? (
+            <>
+              <img
+                src="/images/profile.jpg"
+                className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                alt={name}
+              />
+              <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            </>
+          )
+          : (
+            <>
+              <Link href="/">
+                <a>
+                  <img
+                    src="/images/profile.jpg"
+                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                    alt={name}
+                  />
+                </a>
+              </Link>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/">
+                  <a className={utilStyles.colorInherit}>{name}</a>
+                </Link>
+              </h2>
+            </>
+          )
+        }
+      </header>
+      <main>
+        {children}
+      </main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+};
+
+export default Layout;
