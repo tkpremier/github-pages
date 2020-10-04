064ea1455241defa768d16fa4c9c1091ff48b815
tkpremier <tk@lmbrjck.com>
2020-10-04 14:22:27 -0400

    index update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Create Tommy's App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
