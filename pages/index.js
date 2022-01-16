3d258632d0cb63913d2a617f980b0fb88033e368
tkpremier <kkim31@gmail.com>
2022-01-16 15:23:17 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className="title">
-        Welcome to <Link href="/posts/first-post"><a>TK Premier!</a></Link>
+        Welcome to <Link href="/posts/first-post"><a>T Premier!</a></Link>
       </h1>
 
       <p className="description" onClick={handleClick}>
