92e1976cdf4f8cf0d617d54265b401f022d0a566
tkpremier <kkim31@gmail.com>
2022-07-22 15:57:27 -0400

    update eslinst error

--- pages/model/index.jsx
+++ pages/model/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 // import { format } from "date-fns";
 // import { isNull } from "lodash";
 // import Grid from "../../components/Grid";
-import Layout from "../../components/layout";
+import Layout from '../../components/layout';
 // import handleResponse from "../../utils/handleResponse";
-import { getModelList } from "../../services/db";
+import { getModelList } from '../../services/db';
 
 const getModelFromDatabase = async () => {
   const { data } = await getModelList();
@@ -19,19 +19,17 @@ export async function getServerSideProps() {
   };
 }
 
-const Contacts = props => {
-  return (
-    <Layout drive>
-      <h2>Contacts</h2>
-      <ul>
-        {props.data.map(m => (
-          <li key={m.id}>
-            <Link href={`/model/${m.id}`}>{m.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </Layout>
-  );
-};
+const Contacts = props => (
+  <Layout drive>
+    <h2>Contacts</h2>
+    <ul>
+      {props.data.map(m => (
+        <li key={m.id}>
+          <Link href={`/model/${m.id}`}>{m.name}</Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
 
 export default Contacts;
