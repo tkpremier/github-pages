242f032bedc90e816134d85f3bec65d62f5bfb77
tkpremier <kkim31@gmail.com>
2022-07-22 15:58:20 -0400

    update eslinst error

--- pages/drive/db_legacy.jsx
+++ pages/drive/db_legacy.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { format } from "date-fns";
-import { isNull } from "lodash";
-import Grid from "../../components/Grid";
-import Layout from "../../components/layout";
+import React from 'react';
+import { format } from 'date-fns';
+import { isNull } from 'lodash';
+import Grid from '../../components/Grid';
+import Layout from '../../components/layout';
 // import { getDrive } from "../../services/drive";
-import { getDriveFile } from "../../services/db";
+import { getDriveFile } from '../../services/db';
 
 const getDriveFromDatabase = async () => {
   const { data } = await getDriveFile();
@@ -27,14 +27,12 @@ export async function getServerSideProps() {
   };
 }
 
-const Drive = props => {
-  return (
-    <Layout drive>
-      <h2>Welcome to the &#x1F608;</h2>
-      <p>Here's what we've been up to....</p>
-      <Grid data={props.data} nextPage={props.nextPage} />
-    </Layout>
-  );
-};
+const Drive = props => (
+  <Layout drive>
+    <h2>Welcome to the &#x1F608;</h2>
+    <p>Here's what we've been up to....</p>
+    <Grid data={props.data} nextPage={props.nextPage} />
+  </Layout>
+);
 
 export default Drive;
