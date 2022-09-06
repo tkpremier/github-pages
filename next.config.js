42c380d01ac35cd868f7b06f58c45e6869f9f593
tkpremier <kkim31@gmail.com>
2022-09-06 13:28:07 -0400

    updates to snex.config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
+  swcMinify: true
 };
 
 module.exports = nextConfig;
