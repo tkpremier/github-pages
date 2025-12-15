6282952bfb12d81254617247528dc2f804933e2e
tkpremier <kkim31@gmail.com>
2025-12-15 16:51:00 -0500

    feat(profile): wrapped page with layout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,6 +4,12 @@ import { usePathname, useSearchParams } from 'next/navigation';
 import { useContext, useMemo } from 'react';
 import { UserContext } from '../../src/context/user';
 
+export const UnauthenticatedFallback = ({ loginUrl = '/' }: { loginUrl: string }) => (
+  <h1>
+    <Link href={loginUrl}>Please login</Link>
+  </h1>
+);
+
 const Profile = () => {
   const [user] = useContext(UserContext);
   const pathname = usePathname();
@@ -16,17 +22,7 @@ const Profile = () => {
     }`;
   }, [pathname, searchParams]);
 
-  return user ? (
-    <div>
-      <h1>{user.name}</h1>
-    </div>
-  ) : (
-    <div>
-      <h1>
-        <Link href={loginUrl}>Please login</Link>
-      </h1>
-    </div>
-  );
+  return <div>{user ? <h1>{user.name}</h1> : <UnauthenticatedFallback loginUrl={loginUrl} />}</div>;
 };
 
 export default Profile;
